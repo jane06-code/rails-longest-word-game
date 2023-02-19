85a5ff70e91be89c73071d7dedd74613d84a71fa
jane eme <janeeme23@gmail.com>
2023-02-19 21:23:45 +0100

    longest

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U)
+
+  def new
+    # build the letter
+    @letters = Array.new(10) { ('a'..'z').to_a[rand(26)] }
+    @letters.shuffle!
+  end
+
+  def score
+    # retrieve game data from form
+    # @letters = params [:letter].split('')
+
+      @letters = params[:letters].split
+      @word = (params[:word] || "").upcase
+      @included = included?(@word, @letters)
+      @english_word = english_word?(@word)
+  end
+
+    private
+
+    def included?(word, letters)
+      word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+    end
+
+    def english_word?(word)
+      response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+      json = JSON.parse(response.read)
+      json['found']
+    end
+  
+  #  generate_letters(letter_size)
+  # end
+end
